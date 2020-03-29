1714b80375a73948363d2a7f2359c3bc5c045a3e
BeginnerRudy <mrj9990123@gmail.com>
2020-03-30 10:13:21 +1100

    Formatting the code

--- src/test.py
+++ src/test.py
@@ -4,24 +4,54 @@ import numpy as np
 from collections import defaultdict
 from collections import Counter
 import operator
+import argparse
 
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
 
-tinnyTweetsReader = TweetReader("../data/smallTwitter.json", rank, size)
-tinnyTweets = tinnyTweetsReader.read_line_skip_header()
+def main():
+    """
+    This function is used to combine all single steps together to do the multi-process parallel computing.
+    """
 
-# counter = Counter()
-hashtag_counter = defaultdict(int)
-lang_counter = defaultdict(int)
-for tweet in tinnyTweets:
-    ha = TweetReader.get_hash_tag(tweet['doc']['text'])
-    for h in ha:
-        hashtag_counter[h] += 1
-    lang_counter[TweetReader.get_lang_code(tweet)] += 1
+    # specify the parser usage
+    parser = argparse.ArgumentParser(usage='mpirun -n <number of processes>> python3 <python file> <tweet file path>')
+    parser.add_argument('twitterPath', type=str)
 
-sorted_x = sorted(hashtag_counter.items(), key=operator.itemgetter(1), reverse=True)
-sorted_lang = sorted(lang_counter.items(), key=operator.itemgetter(1), reverse=True)
-print(sorted_x)
-print(sorted_lang)
+    # create the parser
+    args = parser.parse_args()
+
+    # create a job
+    job = Job(args.twitterPath, MPI.COMM_WORLD)
+
+    # execute the job
+    job.exec()
+
+
+class Job:
+
+    def __init__(self, tweets_file_path, comm):
+        self.tweets_file_path = tweets_file_path
+        self.comm = comm
+        self.rank = comm.Get_rank()
+        self.size = comm.Get_size()
+
+    def exec(self):
+        tinny_tweets_reader = TweetReader("../data/smallTwitter.json", self.rank, self.size)
+        tinny_tweets = tinny_tweets_reader.read_line_skip_header()
+
+        # counter = Counter()
+        hashtag_counter = defaultdict(int)
+        lang_counter = defaultdict(int)
+        for tweet in tinny_tweets:
+            ha = TweetReader.get_hash_tag(tweet['doc']['text'])
+            for h in ha:
+                hashtag_counter[h] += 1
+            lang_counter[TweetReader.get_lang_code(tweet)] += 1
+
+        sorted_x = sorted(hashtag_counter.items(), key=operator.itemgetter(1), reverse=True)
+        sorted_lang = sorted(lang_counter.items(), key=operator.itemgetter(1), reverse=True)
+        print(sorted_x[:10])
+        print(sorted_lang[:10])
+
+
+if __name__ == "__main__":
+    main()
